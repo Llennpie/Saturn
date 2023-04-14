59485ce977bdb26937b6623055bab856366c190e
gmm <mulisandou80@gmail.com>
2023-04-13 19:19:20 -0500

    fixing compile errors, dont bother compiling

--- levels/thi/texture_include.inc.c
+++ levels/thi/texture_include.inc.c
@@ -1,141 +1,131 @@
-Gfx thi_dl__5FF599E0_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__5FF599E0_c_rgba16[] = {
-	#include "levels/thi/_5FF599E0_c.rgba16.inc.c"
+Gfx thi_dl_custom_5FF599E0_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_5FF599E0_c_rgba16[] = {
+	#include "levels/thi/custom_5FF599E0_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__351416D1_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__351416D1_c_rgba16[] = {
-	#include "levels/thi/_351416D1_c.rgba16.inc.c"
+Gfx thi_dl_custom_351416D1_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_351416D1_c_rgba16[] = {
+	#include "levels/thi/custom_351416D1_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__1213CD5D_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__1213CD5D_c_rgba16[] = {
-	#include "levels/thi/_1213CD5D_c.rgba16.inc.c"
+Gfx thi_dl_custom_1213CD5D_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_1213CD5D_c_rgba16[] = {
+	#include "levels/thi/custom_1213CD5D_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__161DACD6_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__161DACD6_c_rgba16[] = {
-	#include "levels/thi/_161DACD6_c.rgba16.inc.c"
+Gfx thi_dl_custom_161DACD6_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_161DACD6_c_rgba16[] = {
+	#include "levels/thi/custom_161DACD6_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__643D9B04_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__643D9B04_c_rgba16[] = {
-	#include "levels/thi/_643D9B04_c.rgba16.inc.c"
+Gfx thi_dl_custom_643D9B04_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_643D9B04_c_rgba16[] = {
+	#include "levels/thi/custom_643D9B04_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__3E0B0ACB_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__3E0B0ACB_c_rgba16[] = {
-	#include "levels/thi/_3E0B0ACB_c.rgba16.inc.c"
+Gfx thi_dl_custom_3E0B0ACB_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_3E0B0ACB_c_rgba16[] = {
+	#include "levels/thi/custom_3E0B0ACB_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__5FF581E0_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__5FF581E0_c_rgba16[] = {
-	#include "levels/thi/_5FF581E0_c.rgba16.inc.c"
+Gfx thi_dl_custom_18804702_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_18804702_c_rgba16[] = {
+	#include "levels/thi/custom_18804702_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__18804702_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__18804702_c_rgba16[] = {
-	#include "levels/thi/_18804702_c.rgba16.inc.c"
+Gfx thi_dl_custom_20E3311F_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_20E3311F_c_rgba16[] = {
+	#include "levels/thi/custom_20E3311F_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__20E3311F_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__20E3311F_c_rgba16[] = {
-	#include "levels/thi/_20E3311F_c.rgba16.inc.c"
+Gfx thi_dl_custom_12DFFD64_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_12DFFD64_c_rgba16[] = {
+	#include "levels/thi/custom_12DFFD64_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__12DFFD64_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__12DFFD64_c_rgba16[] = {
-	#include "levels/thi/_12DFFD64_c.rgba16.inc.c"
+Gfx thi_dl_custom_3EC0AAC6_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_3EC0AAC6_c_rgba16[] = {
+	#include "levels/thi/custom_3EC0AAC6_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__3EC0AAC6_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__3EC0AAC6_c_rgba16[] = {
-	#include "levels/thi/_3EC0AAC6_c.rgba16.inc.c"
+Gfx thi_dl_custom_4FC210C5_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_4FC210C5_c_rgba16[] = {
+	#include "levels/thi/custom_4FC210C5_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__4FC210C5_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__4FC210C5_c_rgba16[] = {
-	#include "levels/thi/_4FC210C5_c.rgba16.inc.c"
+Gfx thi_dl_custom_33E8E904_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_33E8E904_c_rgba16[] = {
+	#include "levels/thi/custom_33E8E904_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__33E8E904_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__33E8E904_c_rgba16[] = {
-	#include "levels/thi/_33E8E904_c.rgba16.inc.c"
+Gfx thi_dl_custom_4B32D8F4_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_4B32D8F4_c_rgba16[] = {
+	#include "levels/thi/custom_4B32D8F4_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__4B32D8F4_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__4B32D8F4_c_rgba16[] = {
-	#include "levels/thi/_4B32D8F4_c.rgba16.inc.c"
+Gfx thi_dl_custom_4FF19CCF_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_4FF19CCF_c_rgba16[] = {
+	#include "levels/thi/custom_4FF19CCF_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__4FF19CCF_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__4FF19CCF_c_rgba16[] = {
-	#include "levels/thi/_4FF19CCF_c.rgba16.inc.c"
+Gfx thi_dl_custom_397247A_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_397247A_c_rgba16[] = {
+	#include "levels/thi/custom_397247A_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__4B3248F4_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__4B3248F4_c_rgba16[] = {
-	#include "levels/thi/_4B3248F4_c.rgba16.inc.c"
+Gfx thi_dl_custom_3126057E_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_3126057E_c_rgba16[] = {
+	#include "levels/thi/custom_3126057E_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__397247A_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__397247A_c_rgba16[] = {
-	#include "levels/thi/_397247A_c.rgba16.inc.c"
+Gfx thi_dl_custom_6288AA51_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_6288AA51_c_rgba16[] = {
+	#include "levels/thi/custom_6288AA51_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__3126057E_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__3126057E_c_rgba16[] = {
-	#include "levels/thi/_3126057E_c.rgba16.inc.c"
+Gfx thi_dl_custom_182AFF21_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_182AFF21_c_rgba16[] = {
+	#include "levels/thi/custom_182AFF21_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__6288AA51_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__6288AA51_c_rgba16[] = {
-	#include "levels/thi/_6288AA51_c.rgba16.inc.c"
+Gfx thi_dl_custom_4EA83EBF_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_4EA83EBF_c_rgba16[] = {
+	#include "levels/thi/custom_4EA83EBF_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__182AFF21_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__182AFF21_c_rgba16[] = {
-	#include "levels/thi/_182AFF21_c.rgba16.inc.c"
+Gfx thi_dl_custom_111F03A5_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_111F03A5_c_rgba16[] = {
+	#include "levels/thi/custom_111F03A5_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__4EA83EBF_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__4EA83EBF_c_rgba16[] = {
-	#include "levels/thi/_4EA83EBF_c.rgba16.inc.c"
+Gfx thi_dl_custom_53BB335D_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_53BB335D_c_rgba16[] = {
+	#include "levels/thi/custom_53BB335D_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__111F03A5_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__111F03A5_c_rgba16[] = {
-	#include "levels/thi/_111F03A5_c.rgba16.inc.c"
+Gfx thi_dl_custom_21F5F3BC_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_21F5F3BC_c_rgba16[] = {
+	#include "levels/thi/custom_21F5F3BC_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__53BB335D_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__53BB335D_c_rgba16[] = {
-	#include "levels/thi/_53BB335D_c.rgba16.inc.c"
+Gfx thi_dl_custom_5632258C_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_5632258C_c_rgba16[] = {
+	#include "levels/thi/custom_5632258C_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__21F5F3BC_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__21F5F3BC_c_rgba16[] = {
-	#include "levels/thi/_21F5F3BC_c.rgba16.inc.c"
+Gfx thi_dl_custom_22EE210D_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_22EE210D_c_rgba16[] = {
+	#include "levels/thi/custom_22EE210D_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__5632258C_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__5632258C_c_rgba16[] = {
-	#include "levels/thi/_5632258C_c.rgba16.inc.c"
+Gfx thi_dl_custom_57500458_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_57500458_c_rgba16[] = {
+	#include "levels/thi/custom_57500458_c.rgba16.inc.c"
 };
 
-Gfx thi_dl__22EE210D_c_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__22EE210D_c_rgba16[] = {
-	#include "levels/thi/_22EE210D_c.rgba16.inc.c"
-};
-
-Gfx thi_dl__0_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl__0_rgba16_rgba16[] = {
-	#include "levels/thi/_0_rgba16.rgba16.inc.c"
-};
-
-Gfx thi_dl_castle_grounds_textures_0A800_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
-u8 thi_dl_castle_grounds_textures_0A800_rgba16_rgba16[] = {
-	#include "levels/thi/castle_grounds_textures_0A800_rgba16.rgba16.inc.c"
+Gfx thi_dl_custom_3737DFE1_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 thi_dl_custom_3737DFE1_c_rgba16[] = {
+	#include "levels/thi/custom_3737DFE1_c.rgba16.inc.c"
 };
 
 Gfx thi_dl__02014AB8_rgba16_aligner[] = {gsSPEndDisplayList()};
