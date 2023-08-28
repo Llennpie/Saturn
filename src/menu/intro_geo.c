5143287855effc284e2d715f49f51c2944d9be61
Llennpie <44985633+Llennpie@users.noreply.github.com>
2023-08-27 20:26:49 -0400

    Tweak splash screen background and transitions

--- src/menu/intro_geo.c
+++ src/menu/intro_geo.c
@@ -13,7 +13,7 @@
 #include "gfx_dimensions.h"
 
 // frame counts for the zoom in, hold, and zoom out of title model
-#define INTRO_STEPS_ZOOM_IN 30
+#define INTRO_STEPS_ZOOM_IN 10
 #define INTRO_STEPS_HOLD_1 75
 #define INTRO_STEPS_ZOOM_OUT 91
 
