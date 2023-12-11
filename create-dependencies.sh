0919c4e4c2cb02021e48876bc7bc9c80ddcab5b1
Dominicentek <dominicentekgaming@gmail.com>
2023-12-11 09:09:04 +0100

    redirects in dependency list

--- create-dependencies.sh
+++ create-dependencies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# separate with colon
+# separate with colon, prefix with REDIR to redirect download destination to a separate directory
 DIRS="dynos"
 EXTRA_FILES="fonts/forkawesome-webfont.ttf"
 EXTRA_FILES_WINDOWS="lib/discordsdk/discord_game_sdk.dll"
 EXTRA_FILES_LINUX="lib/discordsdk/libdiscord_game_sdk.so"
+REDIRECTS="lib/discordsdk;."
 
 REPO_OWNER="Llennpie"
 REPO_NAME="Saturn"
@@ -18,6 +19,14 @@ create_dependency_list() {
     for file in "${files[@]}"; do
         echo $file
         echo "$2 https://raw.githubusercontent.com/$REPO_OWNER/$REPO_NAME/$REPO_BRANCH/$file" >> $DEPENDENCY_FILE
+    done
+}
+
+# generate redirect list
+create_redirect_list() {
+    IFS=':' read -ra redirs <<< "$1"
+    for redir in "${redirs[@]}"; do
+        echo "> $redir" >> $DEPENDENCY_FILE
     done
 }
 
@@ -45,6 +54,7 @@ echo "# Prefix lines with # for comments" >> $DEPENDENCY_FILE
 echo "# Prefix lines with $ for Windows only files" >> $DEPENDENCY_FILE
 echo "# Prefix lines with % for Linux only files" >> $DEPENDENCY_FILE
 echo "# Prefix lines with @ for files for both operating systems" >> $DEPENDENCY_FILE
+echo "# Prefix lines with > to redirect downloaded file to a separate directory" >> $DEPENDENCY_FILE
 echo "# Use ./generate-dependencies.sh to create this dependency list" >> $DEPENDENCY_FILE
 echo "" >> $DEPENDENCY_FILE
 
@@ -52,5 +62,6 @@ echo "" >> $DEPENDENCY_FILE
 create_dependency_list $EXTRA_FILES_WINDOWS "$"
 create_dependency_list $EXTRA_FILES_LINUX "%"
 create_dependency_list $EXTRA_FILES "@"
-
 create_dependency_list "$(recursive_search $DIRS)" "@"
+
+create_redirect_list $REDIRECTS
