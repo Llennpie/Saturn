00d13f055de2aa2cffa5e851ab3442a78fcf5ac4
Dominicentek <dominicentekgaming@gmail.com>
2023-12-18 20:41:14 +0100

    forgor to make it a pointer

--- src/pc/gfx/gfx_pc.c
+++ src/pc/gfx/gfx_pc.c
@@ -1970,7 +1970,7 @@ void gfx_precache_textures(void) {
     //fs_walk(FS_TEXTUREDIR, preload_texture, NULL, true);
 
     // ...IM CLEARING THE CACHE INSTEAD XDDDDDDDD
-    memset(gfx_texture_cache, 0, sizeof(gfx_texture_cache));
+    memset(&gfx_texture_cache, 0, sizeof(gfx_texture_cache));
 }
 #endif
 
