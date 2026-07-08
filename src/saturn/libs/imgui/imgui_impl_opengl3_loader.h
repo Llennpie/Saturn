cd45cc3083cb2d2a0a43d270d2bff172f7d21d35
Pracar <97367716+Pracargithub@users.noreply.github.com>
2026-07-08 17:56:27 +0300

    removed opengl3 checker so it would launch lol
    theres prob better way to fix this 
    
    Signed-off-by: Pracar <97367716+Pracargithub@users.noreply.github.com>

--- src/saturn/libs/imgui/imgui_impl_opengl3_loader.h
+++ src/saturn/libs/imgui/imgui_impl_opengl3_loader.h
@@ -677,8 +677,6 @@ static int parse_version(void)
         return GL3W_ERROR_INIT;
     glGetIntegerv(GL_MAJOR_VERSION, &version.major);
     glGetIntegerv(GL_MINOR_VERSION, &version.minor);
-    if (version.major < 3)
-        return GL3W_ERROR_OPENGL_VERSION;
     return GL3W_OK;
 }
 
